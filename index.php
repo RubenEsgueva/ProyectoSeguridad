891549aed810ed010cfdc5cc58171a1e6a461def
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-12 15:51:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
     session_start();
     if (!isset($_SESSION['user_id'])) {
-        header("location: /var/www/html/src/pages/login/login.php");
+        header("location: ./src/pages/login/login.php");
     } else {
-        header("location: /var/www/html/src/pages/catalogo/catalogo.php");
+        header("location: ./src/pages/catalogo/catalogo.php");
     }
 ?>
 <!DOCTYPE html>
