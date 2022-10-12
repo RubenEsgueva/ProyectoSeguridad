054844d47ed8c8d7905088beed03c4cdee1b5821
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-12 15:30:23 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,8 @@
 sudo docker build -t="carshow" .
-sudo docker-compose up
-#firefox "http://localhost:81/index.php"
+sudo docker-compose up -d
+mysql_container_id=$(sudo docker container ls | grep mysql | cut -d " " -f 1)
+sudo docker exec -i $mysql_container_id sh -c 'exec mysql -uadmin -padmin1234' < "$(pwd)/database/database.sql"
+firefox "http://localhost:81/index.php"
+
+#sudo docker-compose ps #Muestra los servivios abiertos en este proyecto
+#sudo docker-compose exec db mysql -uadmin -padmin1234 #Obtener shell de mysql del servidor (obviamente inseguro porue cualquiera puede ejecutar)
