8aeb83c3d873507f293e7a0376a1b1b9afc1661a
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-15 17:29:25 +0200

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
-    if (!isset($_SESSION['user_id'])) {
+    header("location: ./src/pages/catalogo/catalogo.php");
+    /*if (!isset($_SESSION['user_id'])) {
         header("location: ./src/pages/login/login.php");
     } else {
         header("location: ./src/pages/catalogo/catalogo.php");
-    }
+    }*/
 ?>
 <!DOCTYPE html>
 <html lang="es">
