e04fb936065889475b89f8a432cb29e6cc5ea837
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-15 17:33:07 +0200

    Update navbar.php

--- app/src/components/navbar/navbar.php
+++ app/src/components/navbar/navbar.php
@@ -1,7 +1,8 @@
 <?php
     echo '
-    <link rel="stylesheet" href="/var/www/html/src/components/navbar/navbar.css">
+    <link rel="stylesheet" href="/src/components/navbar/navbar.css">
     <nav class="navbar">
-        <img class="menu_icon_white" src="/var/www/html/public/menu_white.png" alt="logo menu">
+        <div></div>
+        <img class="usuario_img_navbar" src="/public/usuario.png" alt="logo menu">
     </nav>';
 ?>
