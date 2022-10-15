0023d346e7637ffa4cb4bdd5b48da318060e1b1a
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-15 19:56:00 +0200

    Update sidebar.js

--- app/src/components/sidebar/sidebar.js
+++ app/src/components/sidebar/sidebar.js
@@ -1,6 +1,8 @@
 let cerrar_sidebar = document.querySelector(".cerrar_sidebar");
 let usuario_img_navbar = document.querySelector(".usuario_img_navbar");
 let sidebar = document.querySelector(".sidebar");
+let cerrar_sesion = document.querySelector(".cerrar_sesion");
+let iniciar_sesion = document.querySelector(".iniciar_sesion");
 
 usuario_img_navbar.addEventListener("click", () => {
     sidebar.style.right = "0";
@@ -8,4 +10,12 @@ usuario_img_navbar.addEventListener("click", () => {
 
 cerrar_sidebar.addEventListener("click", () => {
     sidebar.style.right = "-18em";
+});
+
+cerrar_sesion.addEventListener("click", () => {
+    window.location.replace("http://localhost:81/server/close_session.php");
+});
+
+iniciar_sesion.addEventListener("click", () => {
+    window.location.replace("http://localhost:81/src/pages/login/login.php");
 });
