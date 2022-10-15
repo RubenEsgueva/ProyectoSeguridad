cf91afa2c9f725e2f797706e4cec847ca3bba461
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-15 19:56:26 +0200

    Update sidebar.php

--- app/src/components/sidebar/sidebar.php
+++ app/src/components/sidebar/sidebar.php
@@ -2,7 +2,7 @@
     echo '
     <link rel="stylesheet" href="/src/components/sidebar/sidebar.css">
     <aside class="sidebar">
-        <img class="usuario_img_sidebar" src="/public/usuario.png" alt="Foto de perfil">
+        <img class="usuario_img_sidebar" src="/public/usuario_default.png" alt="Foto de perfil">
         <button class="cerrar_sidebar">Cerrar</button>
         <div class="menu_links">
             <ul>
@@ -10,6 +10,8 @@
                 <li></li>
             </ul>
         </div>
+        <button class="cerrar_sesion">Cerrar Sesión</button>
+        <button class="iniciar_sesion">Iniciar Sesión</button>
     </aside>
     <script src="/src/components/sidebar/sidebar.js"></script>';
 ?>
