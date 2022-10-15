3ea5cc125637b3e80e8c19c3a07fdb3dd24fcc22
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-15 17:31:32 +0200

    Update conexion_db.php

--- app/server/conexion_db.php
+++ app/server/conexion_db.php
@@ -1,9 +1,13 @@
 <?php
+    $hostname = "db";
+    $username = "admin";
+    $password = "admin1234";
+    $db = "COCHES";
 
-$conexion = mysqli_connect("localhost", "root", "", "prueba");
-
-/*if (isset($conexion)) {
-    echo "Conectado";
-}*/
-
+    $conexion = mysqli_connect($hostname, $username, $password, $db);
+    if ($conexion->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+    }
+    //$query = mysqli_query($conexion, "INSERT INTO USUARIOS VALUES('Nombre','Apel','6777777k','988988988','2002-11-28','emailol','passswd','usuario2');")
+    //or die (mysqli_error($conexion));
 ?>
