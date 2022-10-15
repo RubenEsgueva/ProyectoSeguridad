4f1562ced9d2ff7d59b771afcb95d5113cddcb6b
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-15 17:34:59 +0200

    Update catalogo.php

--- app/src/pages/catalogo/catalogo.php
+++ app/src/pages/catalogo/catalogo.php
@@ -1,8 +1,5 @@
 <?php 
     session_start();
-    if (!isset($_SESSION['user_id'])) {
-        header("location: ../../index.php");
-    }
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +9,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/var/www/html/src/pages/catalogo/catalogo.css">
+    <link rel="stylesheet" href="./catalogo.css">
 </head>
 <body>
-    <?php //include '/var/www/html/src/components/navbar/navbar.php'; ?>
-    <?php //include '/var/www/html/src/components/sidebar/sidebar.php'; ?>
+    <?php include '/var/www/html/src/components/navbar/navbar.php'; ?>
+    <?php include '/var/www/html/src/components/sidebar/sidebar.php'; ?>
     <h1>Esto es el Catalogo.</h1>
     <form action="/var/www/html/server/close_session.php" method="post">
         <input type="submit" value="Close">
