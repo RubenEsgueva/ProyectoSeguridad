3e4c44fe74138c33856b72e11978cdedfbc2d8fd
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-16 16:53:32 +0200

    Update sidebar.js

--- app/src/components/sidebar/sidebar.js
+++ app/src/components/sidebar/sidebar.js
@@ -1,15 +1,36 @@
 let cerrar_sidebar = document.querySelector(".cerrar_sidebar");
+let cerrar_sidebar_modificar = document.querySelector(".cerrar_sidebar_modificar");
 let usuario_img_navbar = document.querySelector(".usuario_img_navbar");
 let sidebar = document.querySelector(".sidebar");
+let sidebar_modificar = document.querySelector(".sidebar_modificar");
 let cerrar_sesion = document.querySelector(".cerrar_sesion");
 let iniciar_sesion = document.querySelector(".iniciar_sesion");
+let modificar_datos = document.querySelector(".modificar_datos");
+let cancelar_modificacion = document.querySelector(".cancelar_modificacion");
+let aceptar_modificacion = document.querySelector(".aceptar_modificacion");
 
 usuario_img_navbar.addEventListener("click", () => {
-    sidebar.style.right = "0";
+    if (window.getComputedStyle(sidebar).getPropertyValue('display') == "flex") {
+        sidebar.style.right = "0";
+    } else {
+        sidebar_modificar.style.right = "0";
+    }
 });
 
 cerrar_sidebar.addEventListener("click", () => {
-    sidebar.style.right = "-18em";
+    if (window.getComputedStyle(sidebar).getPropertyValue('display') == "flex") {
+        sidebar.style.right = "-20em";
+    } else {
+        sidebar_modificar.style.right = "-20em";
+    }
+});
+
+cerrar_sidebar_modificar.addEventListener("click", () => {
+    if (window.getComputedStyle(sidebar).getPropertyValue('display') == "flex") {
+        sidebar.style.right = "-20em";
+    } else {
+        sidebar_modificar.style.right = "-20em";
+    }
 });
 
 cerrar_sesion.addEventListener("click", () => {
@@ -18,4 +39,21 @@ cerrar_sesion.addEventListener("click", () => {
 
 iniciar_sesion.addEventListener("click", () => {
     window.location.replace("http://localhost:81/src/pages/login/login.php");
+});
+
+modificar_datos.addEventListener("click", () => {
+    document.querySelector('.sidebar').style.display = "none";
+    document.querySelector('.sidebar_modificar').style.display = "flex";
+    document.querySelector('.sidebar_modificar').style.right = "0";
+});
+
+cancelar_modificacion.addEventListener("click", () => {
+    document.querySelector('.sidebar').style.display = "flex";
+    document.querySelector('.sidebar_modificar').style.display = "none";
+    //falta llamada al servidor con los datos (o con el form y la redireccion sufiifiente?)
+});
+
+aceptar_modificacion.addEventListener("click", () => {
+    document.querySelector('.sidebar').style.display = "none";
+    document.querySelector('.sidebar_modificar').style.display = "flex";
 });
