9d52496f0f4de7acfcbf7e1f37138bc881f50425
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-16 16:51:21 +0200

    Update modificar_usuario.php

--- app/server/modificar_usuario.php
+++ app/server/modificar_usuario.php
@@ -1,1 +1,19 @@
-
+<?php
+    session_start();
+    include 'conexion_db.php';
+    foreach($_POST as $key => $value)
+    {
+        if (!empty($value)) {
+            $usuario = $_SESSION['usuario'];
+            $query = "UPDATE USUARIOS SET $key = $value WHERE usuario = '$usuario'";
+            $resultado = mysqli_query($conexion,$query);
+            $query = "SELECT * FROM USUARIOS WHERE usuario = '$usuario'";
+            $resultado = mysqli_query($conexion,$query);
+            while($row = mysqli_fetch_array($resultado)) {
+                $_SESSION['user_data'] = $row;
+            }
+        }
+    }
+    include 'close_conexion_db.php';
+    echo '<script type="text/javascript">window.location.replace("http://localhost:81/src/pages/catalogo/catalogo.php");</script>';
+?>
