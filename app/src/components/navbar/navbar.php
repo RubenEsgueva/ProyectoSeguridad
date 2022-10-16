b1f122d521d63fdccd36ebdfd80a7572171be225
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-16 16:52:27 +0200

    Update navbar.php

--- app/src/components/navbar/navbar.php
+++ app/src/components/navbar/navbar.php
@@ -3,6 +3,6 @@
     <link rel="stylesheet" href="/src/components/navbar/navbar.css">
     <nav class="navbar">
         <div></div>
-        <img class="usuario_img_navbar" src="/public/usuario_default.png" alt="logo menu">
+        <img class="usuario_img_navbar" src="" alt="Foto perfil">
     </nav>';
 ?>
