77f09bde09ac1f60530a495005651628e752e239
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-16 12:47:40 +0200

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -16,7 +16,7 @@ CREATE TABLE USUARIOS (
   pswd varchar(20) DEFAULT NULL,
   usuario varchar(20) NOT NULL,
   imagen varchar(120),
-  PRIMARY KEY (usuario),
+  PRIMARY KEY (usuario)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 LOCK TABLES USUARIOS WRITE;
