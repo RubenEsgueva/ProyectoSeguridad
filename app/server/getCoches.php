6736b6e63f479af4cf739fba33a4488fb894ee81
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-16 16:48:05 +0200

    Update getCoches.php

--- app/server/getCoches.php
+++ app/server/getCoches.php
@@ -1,14 +1,4 @@
 <?php
     include 'conexion_db.php';
-    $query = "SELECT * FROM USUARIOS";
-    $resultado = mysqli_query($conexion,$query) or die (mysqli_error($conexion));
-    if ($resultado->num_rows > 0) {
-        while ($fila = mysqli_fetch_array($resultado)) {
-            //echo $fila['DNI'];
-            echo print_r($fila);
-        }
-    } else {
-        echo "No hay datos";
-    }
-    include 'close_conexion_db.php';
+    echo 'TO DO'
 ?>
