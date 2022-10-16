79fd427090482ec8b825a9e85da7041dd2eb1ff1
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-16 22:48:15 +0200

    Update navbar.php

--- app/src/components/navbar/navbar.php
+++ app/src/components/navbar/navbar.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="/src/components/navbar/navbar.css">
     <nav class="navbar">
         <div></div>
-        <form class="anadir_coche" action="/server/anadircoche.php" method="post">
+        <form class="anadir_coche" action="/src/pages/anadircoches/anadircoches.php" method="post">
             <input type="submit" class="boton" value="Añadir Coche">
 	    </form>
         <img class="usuario_img_navbar" src="" alt="Foto perfil">
