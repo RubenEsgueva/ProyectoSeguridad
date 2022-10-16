184e2a77f9779eb3361bbd8df33b6d25b49a97d6
Leire <114664274+leirebe@users.noreply.github.com>
2022-10-16 17:21:20 +0200

    ultima versión

--- app/src/pages/login/login.php
+++ app/src/pages/login/login.php
@@ -2,19 +2,41 @@
     session_start(); //Empezar o reanudar sesion en la pagina actual
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/var/www/html/src/pages/login/login.css">
+    <link rel="stylesheet" href="login.css">
+
 </head>
+
 <body>
-    <form action="/var/www/html/server/login.php" method="post">
-        <input type="text" name="user_id">
-        <input type="submit" value="LogIn">
-    </form>
+    <style>
+        .login{
+            font-weight: bold;
+            max-width: fit-content;
+        }
+    </style>
+         <div class="titulo">
+        
+        </div>
+    <div class="derrape">
+        <div class="imagen">
+            <form action="/var/www/html/server/login.php" method="post">
+                <div class="av">
+                    <input type="text" class ="sinborde" name="user_id">
+                    <br>
+                    <input type="submit" class="login" value="Log In"  size="30">  
+                </div>
+            </form>
+        </div>
+    </div>
     <?php include '/var/www/html/src/components/footer/footer.php'?>
+
+   
 </body>
+
 </html>
