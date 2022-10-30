2100f6bbfb2a10b5fe80294ec07963bba4c92ad9
un4rch <81522320+un4rch@users.noreply.github.com>
2022-10-30 15:17:54 +0100

    Update borrar_coche.php

--- app/server/borrar_coche.php
+++ app/server/borrar_coche.php
@@ -1,11 +1,13 @@
 <?php
+    session_start();
     include 'conexion_db.php';
     $matricula = $_POST['matricula'];
     $usuario = $_SESSION['usuario'];
     $dueno = $_POST['usuario'];
-    if (strcmp($usuario, $dueno) != 0) {
+    if (strcmp($usuario, $dueno) === 0) {
         $query = "DELETE FROM COCHES WHERE matricula = '$matricula'";
         $resultado = mysqli_query($conexion,$query);
+    } else {
     }
     include 'close_conexion_db.php';
     echo '<script type="text/javascript">window.location.replace("http://localhost:81/src/pages/catalogo/catalogo.php");</script>';
