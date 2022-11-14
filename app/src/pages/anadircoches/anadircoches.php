7083b871df151275c854f4487e0e96b8ffa88b76
RubenEsgueva <81328415+RubenEsgueva@users.noreply.github.com>
2022-11-14 13:38:44 +0100

    Add files via upload

--- app/src/pages/anadircoches/anadircoches.php
+++ app/src/pages/anadircoches/anadircoches.php
@@ -1,21 +1,5 @@
 <?php 
     session_start();
-	/*if(isset($_POST) & !empty($_POST) )
-	{
-		if(isset($_POST['CSRF_token']))
-		{
-			if ($_POST['CSRF_token'] == $_SESSION['tokenAna'])
-			{
-				echo "CSRF Token Validation Success";
-			}
-			else
-			{
-				echo "Problem with CSRF Token Validation";
-			}
-		}
-	}
-	$token = md5(uniqid(rand(),true));
-	$_SESSION['tokenAna'] = $token;*/
 ?>
 
 <!DOCTYPE html>
@@ -36,6 +20,7 @@
 		$username = "admin";
 		$password = "admin1234";
 		$db = "COCHES";
+		$origen= "anadircoches";
 	
 		$conexion = mysqli_connect($hostname, $username, $password, $db);
 		if ($conexion->connect_error)
@@ -56,134 +41,134 @@
 		$valido = true;
 		//Cada vez que se pulse el boton de confirmar habrá que comprobar el contenido de cada casilla.
 		if ($_SERVER["REQUEST_METHOD"] == "POST" and $_POST['CSRF_token'] == $_SESSION['tokenAna']) 
+			{
+				if (empty($_POST["model"]))
+				{
+					$modelERR = "Es necesario indicar el modelo del vehículo.";
+					$valido = false;
+				}
+				else
+				{
+					$modelo = test_input($_POST["model"]);
+				}
+	
+				if (empty($_POST["platenum"]))
+				{
+					$matERR = "Es necesario indicar la matrícula del vehículo.";
+					$valido = false;
+				}
+				else
+				{
+					$matricula = test_input($_POST["platenum"]);
+					$query = "SELECT * FROM COCHES WHERE matricula = '{$matricula}'";
+					$resultado = mysqli_query($conexion,$query);
+					if ($resultado->num_rows > 0) 
+					{
+						$matERR = "La matrícula ya está registrada.";
+						$valido = false;
+					}
+					elseif (!preg_match("/^[a-zA-Z0-9]*$/",$matricula))
+					{
+						$matERR = "La matrícula no puede incluir espacios o caracteres especiales.";
+						$valido = false;
+					}
+				}
+	
+				if (empty($_POST["imagen"]))
+				{
+					$imgERR = "Para verlo en el catálogo es necesario adjuntar una imagen.";
+					$valido = false;
+				}
+				else
+				{
+					$exten_permit = array("jpg","jpeg","png","gif");
+					$imagen = test_input($_POST["imagen"]);
+					$exten_img = pathinfo($imagen, PATHINFO_EXTENSION);
+					if (!in_array($exten_img, $exten_permit))
+					{
+						$imgERR = "El archivo adjuntado no tiene una extensión válida.";
+						$valido = false;
+					}
+				}
+	
+				if (empty($_POST["status"]))
+				{
+					$estadoERR = "Por favor especifique el estado del vehículo.";
+					$valido = false;
+				}
+				else
+				{
+					$estado = test_input($_POST["status"]);
+				}
+	
+				if (!empty($_POST["km"]))
+				{
+					$kmtraje = test_input($_POST["km"]);
+					if (!preg_match("/^[0-9]*$/",$kmtraje))
+					{
+						$kmERR = "Por favor exprese el kilometraje en números enteros.";
+						$valido = false;
+					}
+				}
+	
+				if (!empty($_POST["price"]))
+				{
+					$precio = test_input($_POST["price"]);
+					if (!preg_match("/^[0-9]*\.[0-9]{2}$/",$precio))
+					{
+						$precioERR = "Utilize el formato 9999.99 por favor.";
+						$valido = false;
+					}
+				}
+				//si todos los contenidos cumplen las condiciones entonces podremos añadir el elemento a la base de datos.
+				if (empty($modelo) and empty($kilometraje) and empty($precio) and empty($matricula) and empty($imagen) and empty($estado))
+				{
+
+				}
+				else
+				{
+					$enviado="INSERT INTO COCHES VALUES ('{$matricula}', '{$modelo}', '{$usuario}', '{$estado}', '{$kilometraje}', '{$precio}', '{$imagen}')";
+					if ($valido)
+					{
+						$resultado="Se ha anadido correctamente";
+					}
+					else
+					{
+						$resultado="Algun dato tenia un valor inadecuado";
+					}
+					include '/var/www/html/server/addlogs.php';
+					if ($valido)
+					{
+						//primero metemos los datos que son obligatorios.
+						$usuario = $_SESSION['usuario'];
+						$dist = intval($kmtraje);
+						$query = "INSERT INTO COCHES (matricula,modelo,usuario,estado,imagen) VALUES ('{$matricula}', '{$modelo}', '{$usuario}', '{$estado}', '{$imagen}')";
+						if ($conexion->query($query) === TRUE) 
+						{
+							//tras haber creado ya la fila con los datos principales vamos comprobando que datos que se pueden quedar vacíos hay puestos.
+							if (isset($precio))
+							{
+								$query = "UPDATE COCHES SET precio = '{$precio}' WHERE matricula = '{$matricula}'";
+							}
+							if (isset($kmtraje))
+							{
+								$query = "UPDATE COCHES SET kilometraje = '{$kmtraje}' WHERE matricula = '{$matricula}'";
+							}
+							//para que se pueda ver la imagen en la web tenemos que importarla a donde podamos manejarla.
+							$location = "/var/www/html/public/matriculas/{$_POST['matricula']}.png";
+							move_uploaded_file($_FILES['imagen']['tmp_name'], $location);
+							//tras meter toda la información necesaria volvemos a catalogo donde ahora debería aparecer el nuevo vehículo.
+							echo '<script type="text/javascript">window.location.replace("http://localhost:81/src/pages/catalogo/catalogo.php");</script>';
+						} 
+					}
+				}
+				
+		}
+		else
 		{
-			if (empty($_POST["model"]))
-	    	{
-	    		$modelERR = "Es necesario indicar el modelo del vehículo.";
-				$valido = false;
-	    	}
-			else
-			{
-				$modelo = test_input($_POST["model"]);
-			}
-
-			if (empty($_POST["platenum"]))
-	    	{
-	    		$matERR = "Es necesario indicar la matrícula del vehículo.";
-				$valido = false;
-	    	}
-			else
-			{
-				$matricula = test_input($_POST["platenum"]);
-				$query = "SELECT * FROM COCHES WHERE matricula = '{$matricula}'";
-				$resultado = mysqli_query($conexion,$query);
-				if ($resultado->num_rows > 0) 
-				{
-					$matERR = "La matrícula ya está registrada.";
-					$valido = false;
-				}
-				elseif (!preg_match("/^[a-zA-Z0-9]*$/",$matricula))
-				{
-					$matERR = "La matrícula no puede incluir espacios o caracteres especiales.";
-					$valido = false;
-				}
-			}
-
-			if (empty($_POST["imagen"]))
-	    	{
-	    		$imgERR = "Para verlo en el catálogo es necesario adjuntar una imagen.";
-				$valido = false;
-	    	}
-			else
-			{
-				$exten_permit = array("jpg","jpeg","png","gif");
-				$imagen = test_input($_POST["imagen"]);
-				$exten_img = pathinfo($imagen, PATHINFO_EXTENSION);
-	    		if (!in_array($exten_img, $exten_permit))
-	    		{
-	    		    $imgERR = "El archivo adjuntado no tiene una extensión válida.";
-					$valido = false;
-	    		}
-			}
-
-			if (empty($_POST["status"]))
-	    	{
-	    		$estadoERR = "Por favor especifique el estado del vehículo.";
-				$valido = false;
-	    	}
-			else
-			{
-				$estado = test_input($_POST["status"]);
-			}
-
-			if (!empty($_POST["km"]))
-	    	{
-	    		$kmtraje = test_input($_POST["km"]);
-	    		if (!preg_match("/^[0-9]*$/",$kmtraje))
-	    		{
-	    		    $kmERR = "Por favor exprese el kilometraje en números enteros.";
-					$valido = false;
-	    		}
-	    	}
-
-			if (!empty($_POST["price"]))
-	    	{
-	    		$precio = test_input($_POST["price"]);
-	    		if (!preg_match("/^[0-9]*\.[0-9]{2}$/",$precio))
-	    		{
-	    		    $precioERR = "Utilize el formato 9999.99 por favor.";
-					$valido = false;
-	    		}
-	    	}
-			//si todos los contenidos cumplen las condiciones entonces podremos añadir el elemento a la base de datos.
-			if ($valido)
-			{
-				//primero metemos los datos que son obligatorios.
-				$usuario = $_SESSION['usuario'];
-				$dist = intval($kmtraje);
-				$query = "INSERT INTO COCHES (matricula,modelo,usuario,estado,imagen) VALUES ('{$matricula}', '{$modelo}', '{$usuario}', '{$estado}', '{$imagen}')";
-				if ($conexion->query($query) === TRUE) 
-				{
-					//tras haber creado ya la fila con los datos principales vamos comprobando que datos que se pueden quedar vacíos hay puestos.
-					if (isset($precio))
-					{
-						$query = "UPDATE COCHES SET precio = '{$precio}' WHERE matricula = '{$matricula}'";
-						if ($conexion->query($query) === TRUE) 
-						{
-							echo "DATABASE UPDATED SUCCESFULLY";
-						}
-						else
-						{
-							echo "Error: " . $query . "<br>" . $conexion->error;
-						}
-					}
-					if (isset($kmtraje))
-					{
-						$query = "UPDATE COCHES SET kilometraje = '{$kmtraje}' WHERE matricula = '{$matricula}'";
-						if ($conexion->query($query) === TRUE) 
-						{
-							echo "DATABASE UPDATED SUCCESFULLY";
-						}
-						else
-						{
-							echo "Error: " . $query . "<br>" . $conexion->error;
-						}
-					}
-					//para que se pueda ver la imagen en la web tenemos que importarla a donde podamos manejarla.
-					$location = "/var/www/html/public/matriculas/{$_POST['matricula']}.png";
-					if (move_uploaded_file($_FILES['imagen']['tmp_name'], $location)) {
-						echo 'Imagen guardada correctamente';
-					} else {
-						echo 'Error';
-					}
-					//tras meter toda la información necesaria volvemos a catalogo donde ahora debería aparecer el nuevo vehículo.
-					echo '<script type="text/javascript">window.location.replace("http://localhost:81/src/pages/catalogo/catalogo.php");</script>';
-				} 
-				else 
-				{
-					echo "Error: " . $query . "<br>" . $conexion->error;
-				}
-			}
+			$enviado="POST sin token o primer acceso a la pagina";
+			$resultado="el POST no se ha procesado";
+			include '/var/www/html/server/addlogs.php';
 		}
 		$token = md5(uniqid(rand(),true));
 		$_SESSION['tokenAna'] = $token;
