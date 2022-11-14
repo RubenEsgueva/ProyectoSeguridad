d9e49429d6ebf568127a1751be85b99cdd65550e
RubenEsgueva <81328415+RubenEsgueva@users.noreply.github.com>
2022-11-14 13:36:39 +0100

    Add files via upload

---
+++ app/server/addlogs.php
@@ -0,0 +1,13 @@
+<?php
+    $ip=$_SERVER['REMOTE_ADDR'];
+    $puerto=$_SERVER['SERVER_PORT'];
+    if(empty($origen))
+    {
+        $origen="externo";
+    }
+    $userid=$_SESSION['usuario'];
+    $current_user = trim(shell_exec('whoami'));
+    $log  = $_SERVER['REMOTE_ADDR'].'-'.$puerto.'-'.$origen.'-'.$userid.'-'.date("F j, Y, g:i a").'-'.$enviado.'-'.$resultado.PHP_EOL;
+    touch('/var/www/html/logs/logs_'.date("j.n.Y").'.txt');
+    file_put_contents('/var/www/html/logs/logs_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
+?>
