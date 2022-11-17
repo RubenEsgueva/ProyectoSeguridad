d803ee7b7ed6eadb186c7b899eb5bc8b5be64cd6
Leire <114664274+leirebe@users.noreply.github.com>
2022-11-17 16:44:59 +0100

    Add files via upload

--- app/src/pages/detalles/detalles.php
+++ app/src/pages/detalles/detalles.php
@@ -30,6 +30,7 @@
         $query = "SELECT * FROM COCHES WHERE matricula='{$matricula}'";
         $datos = mysqli_query($conexion,$query);
         $row = mysqli_fetch_assoc($datos);
+       //
         echo '<br>'.$row['imagen'].' <br>Modelo:' .$row['modelo'].' <br>Propietario: ' .$row['usuario'].'<br>Matricula: ' .$matricula.'<br>Estado: '.$row['estado'];
         if (!empty($row['kilometraje']))
         {
@@ -46,7 +47,29 @@
         <input type="hidden" name="CSRF_token" value="<?php echo $token; ?>">
         <input type="hidden" name="matricula" value="<?php echo $matricula; ?>">
         <input type="hidden" name="usuario" value="<?php echo $usuario; ?>">
-        <br><input type="submit" class="boton_borrar" value="Borrar Vehículo">
+        <br><input type="submit" class="boton" value="Borrar Vehículo">
         <span class="error"><?php echo $anadirERR;?></span><br>
 	</form>
+    <form action="/server/modificar_detalles.php" method="post">
+        <input type="hidden" name="CSRF_token" value="<?php echo $token; ?>">
+        <input type="hidden" name="matricula" value="<?php echo $matricula; ?>">
+        <input type="hidden" name="usuario" value="<?php echo $usuario; ?>">
+        <br><input type="submit" class="boton" value="Modificar Vehículo">
+        <span class="error"><?php echo $anadirERR;?></span><br>
+    </form>
+
+    <!--?php
+        if ($disable)
+        {
+            echo "<h1>Demasiados intentos de Log In, espere un poco para volver a intentarlo.</h1>";
+            echo '<input type="submit" value="Modificar Vehículo" disabled/>';
+            $_SESSION['intentos']=0;
+        }
+        else
+        {
+            echo '<input type="submit" value="Modificar Vehículo"/>';
+        }
+        ?>
+    -->
+    
 </body>
