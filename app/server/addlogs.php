6fd9c272ef1d22083d9b3a0596747f9b9e644c82
un4rch <81522320+un4rch@users.noreply.github.com>
2022-11-18 10:02:12 +0100

    Update addlogs.php

--- app/server/addlogs.php
+++ app/server/addlogs.php
@@ -8,6 +8,6 @@
     $userid=$_SESSION['usuario'];
     $current_user = trim(shell_exec('whoami'));
     $log  = $_SERVER['REMOTE_ADDR'].'-'.$puerto.'-'.$origen.'-'.$userid.'-'.date("F j, Y, g:i a").'-'.$enviado.'-'.$resultado.PHP_EOL;
-    touch('/var/www/html/logs/logs_'.date("j.n.Y").'.txt');
-    file_put_contents('/var/www/html/logs/logs_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
+    touch('/var/log/apache2/logs_'.date("j.n.Y").'.txt');
+    file_put_contents('/var/log/apache2/logs_'.date("j.n.Y").'.txt', $log, FILE_APPEND);
 ?>
