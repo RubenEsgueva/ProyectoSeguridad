288a7543d9631863d74590a5b3061d8ae2dd514c
un4rch <81522320+un4rch@users.noreply.github.com>
2022-11-18 10:02:37 +0100

    Delete anadircoche.php

--- app/server/anadircoche.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if (!isset($_SESSION['usuario'])) {
-        echo '<script type="text/javascript">window.location.replace("http://localhost:81/src/pages/catalogo/catalogo.php");</script>';
-    }
-    //TO DO: Consulta sql INSERT nuevo coche con los datos que se pasan
-?>
