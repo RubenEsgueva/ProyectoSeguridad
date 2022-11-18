e5e89224c7d3ad8d72641ffa5f04b6b30016dfb5
un4rch <81522320+un4rch@users.noreply.github.com>
2022-11-18 10:27:16 +0100

    Update backup.sh

--- backups/backup.sh
+++ backups/backup.sh
@@ -2,7 +2,7 @@
 hoy=$(date -d "today" '+%d-%m-%Y')
 ayer=$(date -d "yesterday" '+%d-%m-%Y')
 
-rsync -av --link-dest=/var/backups/$ayer/basedatos ¿...? /var/backups/$hoy/basedatos
-rsync -av --link-dest=/var/backups/$ayer/logs ¿...? /var/backups/$hoy/logs
+rsync -av --link-dest=/var/backups/$ayer/basedatos /var/lib/mysql/COCHES /var/backups/$hoy/basedatos
+rsync -av --link-dest=/var/backups/$ayer/logs /var/log/apache2 /var/backups/$hoy/logs
 
 #if $hoy es domingo, entonces subir el backup a drive o github o etc...
