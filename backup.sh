03b291ee6c2b5b01511b0afbdf5295c5c07b5bd0
un4rch <81522320+un4rch@users.noreply.github.com>
2022-11-19 17:28:54 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -2,4 +2,10 @@
 hoy=$(date -d "today" '+%d-%m-%Y')
 ayer=$(date -d "yesterday" '+%d-%m-%Y')
 
-rsync -av --link-dest=/var/tmp/Carshow-Backups/$ayer . /var/tmp/Carshow-Backups/$hoy
+rsync -av --link-dest=$(pwd)/backups/$ayer/basedatos $(pwd)/mysql/COCHES $(pwd)/backups/$hoy/basedatos
+rsync -av --link-dest=$(pwd)/backups/$ayer/logs $(pwd)/logs $(pwd)/backups/$hoy/logs
+
+diaSem=$(date -d "today" | cut -d " " -f 1)
+if [[ "$diaSem" == "dom" ]]; then
+    #Implementar subir el backup a un servicio en la nube
+fi
