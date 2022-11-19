1f39e60ede0491d3813d59dec7a3395a42e2dd2f
un4rch <81522320+un4rch@users.noreply.github.com>
2022-11-19 17:29:25 +0100

    Delete backups directory

--- backups/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-hoy=$(date -d "today" '+%d-%m-%Y')
-ayer=$(date -d "yesterday" '+%d-%m-%Y')
-
-rsync -av --link-dest=/var/backups/$ayer/basedatos /var/lib/mysql/COCHES /var/backups/$hoy/basedatos
-rsync -av --link-dest=/var/backups/$ayer/logs /var/log/apache2 /var/backups/$hoy/logs
-
-#if $hoy es domingo, entonces subir el backup a drive o github o etc...
